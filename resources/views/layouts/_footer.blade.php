c78e024d0ce305c6ce8fb8665ff6a5eaf5272ab5
livephone1988 <livephone1988@163.com>
2019-02-01 12:18:18 +0800

    切割头部尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+	<img class="brand-icon" src="https://www.easyicon.net/api/resizeApi.php?id=2402&size=128">
+	<a href="http://www.netoper.com" target="_blank">
+		坚持提高自己
+	</a>
+	<div class="float-right">
+		<a href="/about">关于</a>
+	</div>
+</footer>
