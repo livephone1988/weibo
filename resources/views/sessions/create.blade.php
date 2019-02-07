fd79e94c19c517bbfc52dd56e67caa93235f6609
livephone <livephone1988@163.com>
2019-02-07 18:57:15 +0000

    记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -21,6 +21,15 @@
 					<label for="password">密码：</label>
 					<input type="password" name="password" class="form-control" value="{{ old('password') }}" >	
 				</div>
+
+				<div class="form-group">
+					<div class="form-check">
+						<input type="checkbox" class="form-check-input" name="remeber" id="exampleCheck1">
+						<label class="form-check-label" for="exampleCheck1">记住我</label>
+					</div>
+				</div>
+
+
 					<button type="sublime" class="btn btn-primary">登录</button>
 			</form>	
 			<hr>
