4bd48caaf813d172c70b734b7b758625485c79e6
livephone <livephone1988@163.com>
2019-02-10 19:43:04 +0000

    修复无法发微博的问题

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -10,19 +10,23 @@ class StatusesController extends Controller
 {
     public function __construct()
     {
-    	$this->middleware('auth');
+        $this->middleware('auth');
     }
+
     public function store(Request $request)
     {
-    	$this->validate($request,[
-    		'content' => 'required|max:140'
-    	]);
-    	Auth::user()->statuses()->create([
-    		'content' => $request['content']
-    	]);
-    	session()->flash('success','发布成功！');
-    	return redirect()->back();
+        $this->validate($request, [
+            'content' => 'required|max:140'
+        ]);
+
+        Auth::user()->statuses()->create([
+            'content' => $request['content']
+        ]);
+        session()->flash('success', '发布成功！');
+        return redirect()->back();
     }
+
+
     public function destroy(Status $status)
     {
         $this->authorize('destroy', $status);
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.default')
-@section('content')
-	@if (Auth::check())
 
-		<div class="row">
-			<div class="col-md-8">
-				<section class="status_form">
-					@include('shared._status_form')	
-				</section>	
-				<h4>微博列表</h4>
-				<hr>
-				@include('shared._feed')
-			</div>	
-			<aside class="col-md-4">
-				<section class="user_info">
-					@include('shared._user_info',['user' => Auth::user()])
-				</section>
-			</aside>
-		</div>
-	@else
+@section('content')
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+        <h4>微博列表</h4>
+        <hr>
+        @include('shared._feed')
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
 		<div class="jumbotron">
 			<h1>Hey Guys</h1>
 			<p class="lead">
