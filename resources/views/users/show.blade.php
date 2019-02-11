edda7bb357c85c621a05c2401322a07421ff0804
livephone <livephone1988@163.com>
2019-02-11 07:20:20 +0000

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,10 +3,14 @@
 
 @section('content')
 <div class="row">
-  <div class="offset-md-2 col-md-8">
+  <div class="offset-md-2 col-md-10">
     <section class="user_info">
       @include('shared._user_info', ['user' => $user])
     </section>
+    <section class="stats mt-3">
+      @include('shared._stats', ['user' => $user])
+    </section>
+    <hr>
     <section class="status">
       @if ($statuses->count() > 0)
         <ul class="list-unstyled">
